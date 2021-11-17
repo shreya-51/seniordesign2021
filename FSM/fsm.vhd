d9ae45d0733296b95e5278f0d411d955504521ee
gih6 <54678622+gih6@users.noreply.github.com>
2021-11-16 21:33:08 -0500

    Update fsm.vhd

--- FSM/fsm.vhd
+++ FSM/fsm.vhd
@@ -8,12 +8,14 @@ use ieee.std_logic_1164.all;
 entity FSm is
 
 	port(
-		CLK		 : in	std_logic;
-		DATA     : in   std_logic;
-		RESET    : in   std_logic;
-		COUNTER  : in   std_logic_vector(4 downto 0);
+		CLK		  : in	std_logic;
+		DATA        : in   std_logic;
+		RESET         : in   std_logic;
+		COUNTER    : in   std_logic_vector(4 downto 0);
+		COUNTERSWE:in  std_logic_vector(5 downto 0);
 		enableD  : out  std_logic;
 		enableR  : out  std_logic;
+		resetSWE : out  std_logic;
 		DONE     : out  std_logic      
 	);
 
@@ -22,7 +24,7 @@ end entity;
 architecture rtl of FSm is
 
 	-- Build an enumerated type for the state machine
-	type state_type is (s0, s1, s2, s3);
+	type state_type is (s0, s1, s2, s3,s4);
 
 	-- Register to hold the current state
 	signal state   : state_type;
@@ -41,11 +43,17 @@ begin
 				when s1=> -- 1st to 31st clock cycle // read in the data 
 					 if(counter = "11101") then -- 31st clock cycle stop and add done signal 
 					   state <= s2;
+					 elsif(counter = "00001" and DATA = '1')then
+					  state <= s4;
 					 end if;
 				when s2=> -- loading up data 
 					   state <= s3;
 			    when s3 => 
 			         state <= s1;
+			    when s4 => -- here getting data for the SWEEP FUNCTION!!! 
+			          if(counterSWE = "111110") then -- 31st clock cycle stop and add done signal 
+			             state <= s1;
+			          end if;
 			     when others => 
                     state <= s0;
 			end case;
@@ -65,6 +73,7 @@ begin
 			   DONE <= '0';
 			   enableD <= '1';
 			   enableR <= '0';
+			   resetSWE <= '0';
 			when s2 =>
                 DONE <= '0';
                 enableD <= '1';
@@ -72,6 +81,11 @@ begin
             when s3 =>
                 DONE <= '1';
                 enableR <= '0';
+                resetSWE <= '1';
+            when s4 =>
+                DONE <= '1';
+                enableR <= '0';
+                resetSWE <= '0';
             when others =>
                 DONE <= '0';
 		end case;
