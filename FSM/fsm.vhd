bf1dc83325c7a589ba5781f5a81ba7b86aa95846
gih6 <54678622+gih6@users.noreply.github.com>
2021-11-17 12:37:42 -0500

    Update fsm.vhd

--- FSM/fsm.vhd
+++ FSM/fsm.vhd
@@ -15,8 +15,10 @@ entity FSm is
 		COUNTERSWE:in  std_logic_vector(5 downto 0);
 		enableD  : out  std_logic;
 		enableR  : out  std_logic;
+		enableS  : out  std_logic;
+		enableSWER: out std_logic;
 		resetSWE : out  std_logic;
-		rstReg   : out  std_logic;
+		rstReg : out std_logic;
 		DONE     : out  std_logic      
 	);
 
@@ -25,7 +27,7 @@ end entity;
 architecture rtl of FSm is
 
 	-- Build an enumerated type for the state machine
-	type state_type is (s0, s1, s2, s3,s4);
+	type state_type is (s0, s1, s2, s3,s4,s5,s6);
 
 	-- Register to hold the current state
 	signal state   : state_type;
@@ -41,7 +43,11 @@ begin
 		elsif (rising_edge(clk)) then
 			case state is
 				when s0=> --reset stage (32nd Clock Cycle) 
+					 if(counter = "00000" and DATA = '1')then
+					  state <= s4;
+					 end if;
 					state <= s1;
+					resetReg <= '0';
 				when s1=> -- 1st to 31st clock cycle // read in the data 
 					 if(counter = "11101") then -- 31st clock cycle stop and add done signal 
 					   state <= s2;
@@ -54,11 +60,15 @@ begin
 					   state <= s3;
 			    when s3 => 
 			         resetReg <= '0';
-						state <= s1;
-			    when s4 => -- here getting data for the SWEEP FUNCTION!!! 
-			          if(counterSWE = "111110") then -- 31st clock cycle stop and add done signal 
-			             state <= s1;
+			         state <= s1;
+			    when s4 => -- here getting data for the SWEEP FUNCTION!!! 111110
+			          if(counterSWE = "111101") then -- 63rd clock cycle stop and add done signal 
+			             state <= s5;
 			          end if;
+			   when s5 => -- here getting data for the SWEEP FUNCTION!!! 111110
+			         state <= s6;
+			   when s6 =>
+			         state <= s1;
 			     when others => 
                     state <= s0;
 			end case;
@@ -73,27 +83,39 @@ begin
 			when s0 =>
 			   DONE <= '0';
 			   enableD <= '1';
+			   enableS <= '1';
 			   enableR <= '0';
+			   enableSWER <= '0';
+			   Rstreg <= resetReg;
 			when s1 =>      
 			   DONE <= '0';
 			   enableD <= '1';
+			   enableS <= '1';
 			   enableR <= '0';
 			   resetSWE <= '0';
-				rstReg <= resetReg;
+			   enableSWER <= '0';
+			   Rstreg <= resetReg;
 			when s2 =>
                 DONE <= '0';
                 enableD <= '1';
                 enableR <= '1';
-					 rstReg <= resetReg;
+                Rstreg <= resetReg;
             when s3 =>
                 DONE <= '1';
                 enableR <= '0';
                 resetSWE <= '1';
-					 rstReg <= resetReg;
+                Rstreg <= resetReg;
             when s4 =>
                 DONE <= '1';
                 enableR <= '0';
                 resetSWE <= '0';
+                enableS <= '1';
+                Rstreg <= resetReg;
+                enableSWER <= '1';
+             when s5 =>
+              enableSWER <= '0';
+             when s6 =>
+              enableSWER <= '0';
             when others =>
                 DONE <= '0';
 		end case;
