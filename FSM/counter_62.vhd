06aee0bbb55d66aea8b165a71664f3e8e01f861b
gih6 <54678622+gih6@users.noreply.github.com>
2021-11-16 21:32:43 -0500

    Create counter_62.vhd

---
+++ FSM/counter_62.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+
+entity counter_62 is
+    port (
+        cout   :out std_logic_vector (5 downto 0); 
+        enable :in  std_logic;                     
+        clk    :in  std_logic;                     
+        reset  :in  std_logic                      
+     );
+ end entity;
+ 
+ architecture rtl of counter_62 is
+     signal count :std_logic_vector (5 downto 0) := "000000";
+ begin
+     process (clk, reset) begin
+         if (reset = '1') then
+             count <= (others=>'0');
+        elsif (rising_edge(clk)) then
+            if (enable = '1') then
+                  count <= count + 1;
+             end if;
+          end if;
+      end process;
+     cout <= count;
+end architecture;
