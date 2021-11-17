954f94b04ecd27ecd6c5a737c191918a903a4137
gih6 <54678622+gih6@users.noreply.github.com>
2021-11-17 11:12:50 -0500

    Update fsm.vhd

--- FSM/fsm.vhd
+++ FSM/fsm.vhd
@@ -15,9 +15,8 @@ entity FSm is
 		COUNTERSWE:in  std_logic_vector(5 downto 0);
 		enableD  : out  std_logic;
 		enableR  : out  std_logic;
-		enableS  : out  std_logic;
 		resetSWE : out  std_logic;
-		rstReg : out std_logic;
+		rstReg   : out  std_logic;
 		DONE     : out  std_logic      
 	);
 
@@ -43,7 +42,6 @@ begin
 			case state is
 				when s0=> --reset stage (32nd Clock Cycle) 
 					state <= s1;
-					resetReg <= '0';
 				when s1=> -- 1st to 31st clock cycle // read in the data 
 					 if(counter = "11101") then -- 31st clock cycle stop and add done signal 
 					   state <= s2;
@@ -56,7 +54,7 @@ begin
 					   state <= s3;
 			    when s3 => 
 			         resetReg <= '0';
-			         state <= s1;
+						state <= s1;
 			    when s4 => -- here getting data for the SWEEP FUNCTION!!! 
 			          if(counterSWE = "111110") then -- 31st clock cycle stop and add done signal 
 			             state <= s1;
@@ -76,29 +74,26 @@ begin
 			   DONE <= '0';
 			   enableD <= '1';
 			   enableR <= '0';
-			   Rstreg <= resetReg;
 			when s1 =>      
 			   DONE <= '0';
 			   enableD <= '1';
 			   enableR <= '0';
 			   resetSWE <= '0';
-			   Rstreg <= resetReg;
+				rstReg <= resetReg;
 			when s2 =>
                 DONE <= '0';
                 enableD <= '1';
                 enableR <= '1';
-                Rstreg <= resetReg;
+					 rstReg <= resetReg;
             when s3 =>
                 DONE <= '1';
                 enableR <= '0';
                 resetSWE <= '1';
-                Rstreg <= resetReg;
+					 rstReg <= resetReg;
             when s4 =>
                 DONE <= '1';
                 enableR <= '0';
                 resetSWE <= '0';
-                enableS <= '1';
-                Rstreg <= resetReg;
             when others =>
                 DONE <= '0';
 		end case;
