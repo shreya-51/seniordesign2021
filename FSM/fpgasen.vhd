2b3ca8e5a38020ae27df5c2787d2d73c3b7cfd14
gih6 <54678622+gih6@users.noreply.github.com>
2021-12-01 20:36:02 -0500

    Update fpgasen.vhd

--- FSM/fpgasen.vhd
+++ FSM/fpgasen.vhd
@@ -1,4 +1,42 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
 
+
+entity regFile is
+  Port (
+    clk         :   in std_logic;
+    Channel     :   in  std_logic_vector(2 downto 0);
+    dataIn     :    in std_logic_vector(11 downto 0); 
+    reset       :   in  std_logic;
+    enable      :   in  std_logic;
+    dataOut1     :   out std_logic_vector(11 downto 0); 
+    dataOut2     :   out std_logic_vector(11 downto 0);
+    dataOut3     :   out std_logic_vector(11 downto 0)
+  );
+end regFile;
+
+architecture Behavioral of regFile is
+  type t_regF is array(0 to 7) of std_logic_vector(11 downto 0);
+  signal regFArray : t_regF;
+begin
+    process(clk) is
+    begin
+    if (rising_edge(clk)) then
+            if(reset = '1' and enable = '1') then -- have to check if reset to clear to make it all equal 0
+                regFArray <= (others =>(others =>'0')); -- make all registers = 0
+               --Check if ELSEIF ENABLE W = 1 you WRITE REGISTER
+             elsif(enable = '1') then
+                regFArray(to_integer(unsigned(Channel))) <=  dataIn;
+            end if;
+    end if;
+    end process;
+    
+    dataOut1 <= regFArray(0);
+    dataOut2 <= regFArray(1);
+    dataOut3 <= regFArray(2);
+end Behavioral;
+	    
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -234,7 +272,7 @@ entity fpgasen is
  H4	  :		out	  std_logic_vector(0 to 6);
  H5	  :		out	  std_logic_vector(0 to 6);
  H6	  :		out	  std_logic_vector(0 to 6);
- Chan1  :      out     std_logic_Vector(11 downto 0);
+ data_out_0  :      out     std_logic_Vector(11 downto 0);
  Chan2  :      out     std_logic_Vector(11 downto 0);
  Chan3  :      out     std_logic_Vector(11 downto 0);
  LED0		:		out	  std_logic;
@@ -390,4 +428,5 @@ bcd6:bcd7seg PORT MAP (B => data3_sig(7 downto 4), display => H5);
 --bcd6:bcd7seg PORT MAP (B => data3_sig(7 downto 4), display => H5);
 led0 <= resetReg;
 led1 <= enableR;
+data_out_0 <= data1_sig;
 end Behavioral;
